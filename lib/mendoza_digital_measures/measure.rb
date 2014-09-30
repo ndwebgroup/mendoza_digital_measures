d007014657c107132fe9efb840a91e74cde662e0
Jonathan Arp <jarp@nd.edu>
2014-09-30 15:35:55 -0400

    added corey angst data for other lists adding more keys to the hash for lists of things

--- lib/mendoza_digital_measures/measure.rb
+++ lib/mendoza_digital_measures/measure.rb
@@ -15,14 +15,16 @@ module MendozaDigitalMeasures
         last_name: @measure.xpath("//PCI/LNAME").first.text,
         middle_name: @measure.xpath("//PCI/MNAME").first.text,
         email: @measure.xpath("//PCI/EMAIL").first.text,
+        website: @measure.xpath("//PCI/WEBSITE").first.text,
         bio: @measure.xpath("//PCI/BIO").first.text,
         room_number: @measure.xpath("//PCI/ROOMNUM").first.text,
         phone: [@measure.xpath("//PCI/OPHONE1").first.text, @measure.xpath("//PCI/OPHONE2").first.text, @measure.xpath("//PCI/OPHONE3").first.text ].join("-"),
-        title: "",
+        title:  @measure.xpath("//ADMIN/RANK").first.text,
         endowed_position: @measure.xpath("//PCI/ENDPOS").first.text ,
 
         areas_of_expertise: areas_of_expertise,
         education: education,
+        publications: publications,
         teaching: teaching
       }
     end
@@ -31,18 +33,36 @@ module MendozaDigitalMeasures
 
     def areas_of_expertise
       expertisei = []
-      @measure.xpath("//PCI/PCI_EXPERTISE/EXPERTISE").each do | e |
-        expertisei << e.text
-      end
+        @measure.xpath("//PCI/PCI_EXPERTISE/EXPERTISE").each do | e |
+          expertisei << e.text
+        end
       return expertisei
     end
 
     def education
+      educations = []
+      @measure.xpath("//EDUCATION").each do | e |
+        educations << "#{e.xpath("DEG").first.text}, #{e.xpath("SCHOOL").first.text}"
+      end
+      return educations
     end
 
 
     def teaching
-      []
+      teachings = []
+      @measure.xpath("//SCHTEACH").each do | e |
+        teachings << "#{e.xpath("TITLE").first.text}"
+      end
+      teachings.uniq!
+      return teachings
+    end
+
+    def publications
+      items = []
+      @measure.xpath("//INTELLCONT").each do | n |
+        items << "#{n.xpath("TITLE").first.text}"
+      end
+      return items
     end
 
 
--- test/faculty_measure_test.rb
+++ test/faculty_measure_test.rb
@@ -9,28 +9,75 @@ describe MendozaDigitalMeasures::Measure do
   let(:jaffleck_response) { Typhoeus::Response.new(code: 200, body: fixture_xml('jaffleck')) }
   let(:failed_response) { Typhoeus::Response.new(code: 404, body: '')  }
 
-  before(:each) do
-    subject = MendozaDigitalMeasures::Measure.new(fixture_xml('cackerm1'))
-    @faculty = subject.to_hash
+
+  describe "with C Ackerman data" do
+    before(:each) do
+      subject = MendozaDigitalMeasures::Measure.new(fixture_xml('cackerm1'))
+      @faculty = subject.to_hash
+    end
+
+    it 'converts the response to a hash' do
+      @faculty.must_be_kind_of Hash
+    end
+
+    it 'pulls in personal information' do
+      @faculty[:first_name].must_equal "Carl"
+      @faculty[:last_name].must_equal "Ackermann"
+      @faculty[:phone].must_equal "574-631-8407"
+      @faculty[:email].must_equal "Ackermann.1@nd.edu"
+      @faculty[:title].must_equal "Professional Specialist"
+      @faculty[:endowed_position].must_equal "Nolan Professorship for Excellence in Undergraduate Instruction"
+    end
+
+    it "pulls in an array of expertise as area of expertise" do
+      @faculty[:areas_of_expertise].must_be_kind_of Array
+      @faculty[:areas_of_expertise].count.must_equal 3
+      @faculty[:areas_of_expertise].first.must_equal "Mutual Funds"
+      @faculty[:areas_of_expertise].last.must_equal "Personal Finance"
+    end
+
+    it "pulls in an array of education" do
+      @faculty[:education].must_be_kind_of Array
+      @faculty[:education].count.must_equal 2
+      #puts @faculty[:education]
+      @faculty[:education].first.must_equal "Ph D, University of North Carolina at Chapel Hill"
+    end
+
+
+    it "pulls in an array of teaching" do
+      @faculty[:teaching].must_be_kind_of Array
+      puts @faculty[:teaching].inspect
+      #todo why does this return four but only show one on site
+      #@faculty[:teaching].count.must_equal 1
+      @faculty[:teaching].first.must_equal "Corporate Financial Management"
+    end
   end
 
-  it 'converts the response to a hash' do
-    @faculty.must_be_kind_of Hash
-  end
+  describe "with Corey Angst Data" do
+    before(:each) do
+      subject = MendozaDigitalMeasures::Measure.new(fixture_xml('cangst'))
+      @corey = subject.to_hash
+    end
 
-  it 'pulls in personal information' do
-    @faculty[:first_name].must_equal "Carl"
-    @faculty[:last_name].must_equal "Ackermann"
-    @faculty[:phone].must_equal "574-631-8407"
-    @faculty[:email].must_equal "Ackermann.1@nd.edu"
-    @faculty[:endowed_position].must_equal "Nolan Professorship for Excellence in Undergraduate Instruction"
-  end
 
-  it "pulls in an array of expertise as area of expertise" do
-    @faculty[:areas_of_expertise].must_be_kind_of Array
-    @faculty[:areas_of_expertise].count.must_equal 3
-    @faculty[:areas_of_expertise].first.must_equal "Mutual Funds"
-    @faculty[:areas_of_expertise].last.must_equal "Personal Finance"
-  end
+    it "pulls in publications" do
+      @corey[:publications].must_be_kind_of Array
+      @corey[:publications].count.must_equal 16
+      @corey[:publications].first.must_equal '<a href="http://onlinelibrary.wiley.com/doi/10.1002/smj.2300/pdf">"The Impact of Culture on the Relationship between Governance and Opportunism in Outsourcing Relationships</a>,"(with Sean  Handley,), To appear in <i>Strategic Management Journal</i>, Forthcoming, 2015.'
+    end
 
+
+    it "pulls in books" do
+      @corey[:books].must_be_kind_of Array
+      @corey[:books].count.must_equal 2
+    end
+
+    it "presentations" do
+      @corey[:presentations].must_be_kind_of Array
+      @corey[:presentations].count.must_equal 29
+    end
+
+
+
+  end
 end
